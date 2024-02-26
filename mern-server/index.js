1fdcc9f104475ecf2f460397d61c25b0956d8771
Ramya Rajeshwaran <150523451+RamyaRajeshwaran@users.noreply.github.com>
2024-02-26 10:26:15 +0530

    Update index.js

--- mern-server/index.js
+++ mern-server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Replace 'http://localhost:5000' with your frontend URL
+const frontendUrl = 'https://book-app-frontend-rho.vercel.app'; 
 
 const uri = "mongodb+srv://mern1-book-store:BjyWvrPBo6K5nVZg@cluster0.227znnb.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
@@ -18,10 +25,6 @@ async function run() {
 
     const db = client.db("BookInventory");
     const bookCollections = db.collection("books");
-
-    app.get("/", (req, res) => {
-      res.send("Welcome to the Book Inventory App");
-    });
 
     app.post("/upload-book", async (req, res) => {
       const data = req.body;
@@ -66,6 +69,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/", (req, res) => {
+      // Redirect to the frontend URL
+      res.redirect(frontendUrl);
+    });
+
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
     });
@@ -75,3 +83,9 @@ async function run() {
 }
 
 run();
+
+   
+      
+      
+
+
